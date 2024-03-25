fb7733b0f419ff023e335e6752fbfe803b7951b4
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-03-25 23:19:57 +0100

    Added entrypoint files

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.once('ready', () => {
+    console.log("Online");
+});
+
+client.login(process.env.TOKEN);
